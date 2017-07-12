412f630840792f23609f01943f91e232057d6762
Pavel Zubkou <pavel.zubkou@gmail.com>
2017-07-12 23:59:40 +0300

    Add styled Button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Button = styled.button`
+  border: 1px solid blue;
+`;
 
 class App extends Component {
   render() {
@@ -13,6 +18,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>Hello</Button>
       </div>
     );
   }
